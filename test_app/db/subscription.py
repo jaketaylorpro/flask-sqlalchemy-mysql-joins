71e396844a55126999b065ad129ea3699e80e895
jaketaylorpro <jaketaylorpro@gmail.com>
2018-10-03 12:38:16 -0400

    figured out how to force joins instead of multiple queries

--- test_app/db/subscription.py
+++ test_app/db/subscription.py
@@ -6,6 +6,6 @@ class Subscription(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
-    product = db.relationship("Product")
+    product = db.relationship("Product", lazy='joined')
     start_date = db.Column(db.DateTime, default=db.func.current_timestamp())
     end_date = db.Column(db.DateTime, default=None)
--- test_app/db/user.py
+++ test_app/db/user.py
@@ -6,7 +6,7 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     username = db.Column(db.String(80), unique=True)
-    subscriptions = db.relationship("Subscription", backref="user")
+    subscriptions = db.relationship("Subscription", lazy='joined')
 
     def to_json(self):
         return dict_to_json(
